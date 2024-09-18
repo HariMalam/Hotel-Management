3bba7170c72b8bbb18b1a67a06a9bf29ce4a209f
Malam Hari <malamharid@gmail.com>
2024-09-18 20:31:21 +0530

    inititial

---
+++ bluebirdhotel.sql
@@ -0,0 +1,225 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1deb3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Sep 17, 2024 at 03:18 PM
+-- Server version: 11.4.3-MariaDB-1
+-- PHP Version: 8.2.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bluebirdhotel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emp_login`
+--
+
+CREATE TABLE `emp_login` (
+  `empid` int(100) NOT NULL,
+  `Emp_Email` varchar(50) NOT NULL,
+  `Emp_Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `emp_login`
+--
+
+INSERT INTO `emp_login` (`empid`, `Emp_Email`, `Emp_Password`) VALUES
+(1, 'Admin@gmail.com', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(30) NOT NULL,
+  `Name` varchar(30) NOT NULL,
+  `Email` varchar(30) NOT NULL,
+  `RoomType` varchar(30) NOT NULL,
+  `Bed` varchar(30) NOT NULL,
+  `NoofRoom` int(30) NOT NULL,
+  `cin` date NOT NULL,
+  `cout` date NOT NULL,
+  `noofdays` int(30) NOT NULL,
+  `roomtotal` double(8,2) NOT NULL,
+  `bedtotal` double(8,2) NOT NULL,
+  `meal` varchar(30) NOT NULL,
+  `mealtotal` double(8,2) NOT NULL,
+  `finaltotal` double(8,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `id` int(30) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `bedding` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`id`, `type`, `bedding`) VALUES
+(32, 'Guest House', 'Double');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roombook`
+--
+
+CREATE TABLE `roombook` (
+  `id` int(10) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Country` varchar(30) NOT NULL,
+  `Phone` varchar(30) NOT NULL,
+  `RoomType` varchar(30) NOT NULL,
+  `Bed` varchar(30) NOT NULL,
+  `Meal` varchar(30) NOT NULL,
+  `NoofRoom` varchar(30) NOT NULL,
+  `cin` date NOT NULL,
+  `cout` date NOT NULL,
+  `nodays` int(50) NOT NULL,
+  `stat` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signup`
+--
+
+CREATE TABLE `signup` (
+  `UserID` int(100) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `id` int(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `work` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`id`, `name`, `work`) VALUES
+(3, 'rohit patel', 'Cook'),
+(4, 'Dipak', 'Cook'),
+(5, 'tirth', 'Helper'),
+(6, 'mohan', 'Helper'),
+(7, 'shyam', 'cleaner'),
+(8, 'rohan', 'weighter'),
+(9, 'hiren', 'weighter'),
+(10, 'nikunj', 'weighter'),
+(11, 'rekha', 'Cook'),
+(12, 'Malam Hari', 'Manager');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `emp_login`
+--
+ALTER TABLE `emp_login`
+  ADD PRIMARY KEY (`empid`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roombook`
+--
+ALTER TABLE `roombook`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `signup`
+--
+ALTER TABLE `signup`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `emp_login`
+--
+ALTER TABLE `emp_login`
+  MODIFY `empid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `roombook`
+--
+ALTER TABLE `roombook`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+
+--
+-- AUTO_INCREMENT for table `signup`
+--
+ALTER TABLE `signup`
+  MODIFY `UserID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.md
@@ -0,0 +1,61 @@
+# Hotel Management System
+
+## Overview
+This project is a Hotel Management System developed using PHP and MySQL. It provides a user-friendly interface for managing hotel bookings, customer details, and room availability.
+
+## Prerequisites
+
+Before you begin, ensure you have the following installed:
+
+- [XAMPP](https://www.apachefriends.org/index.html) (or any other local server that supports PHP and MySQL)
+
+## Installation Instructions
+
+### Step 1: Install XAMPP
+
+1. Download and install XAMPP from the [official website](https://www.apachefriends.org/index.html).
+2. During the installation, ensure that the Apache and MySQL components are selected.
+
+### Step 2: Set Up the Project
+
+1. **Clone the repository or download the project files**:
+   - If using Git, run:
+     ```bash
+     git clone <repository-url>
+     ```
+   - Alternatively, download the ZIP file and extract it.
+
+2. **Copy files to the htdocs directory**:
+   - Locate your XAMPP installation directory (usually `C:\xampp` on Windows).
+   - Navigate to the `htdocs` folder (`C:\xampp\htdocs`).
+   - Copy the entire project folder into `htdocs`. You can rename it to `hotel_management_system` for convenience.
+
+### Step 3: Set Up the Database
+
+1. **Import the database**:
+   - Open XAMPP Control Panel and start Apache and MySQL.
+   - Open your web browser and go to `http://localhost/phpmyadmin`.
+   - Create a new database:
+     - Click on "Databases" and create a new database named `bluebirdhotel`.
+   - Import the SQL file:
+     - Select the `bluebirdhotel` database.
+     - Click on the "Import" tab.
+     - Choose the `bluebirdhotel.sql` file from your project root directory and click "Go".
+
+### Step 4: Start the Server
+
+1. Open your web browser and go to:
+http://localhost/hotel_management_system
+
+
+### Usage
+
+- You can now navigate through the Hotel Management System to manage bookings, view customer details, and check room availability.
+
+## Credits
+
+This project was created by **Malam Hari**. Special thanks for their contributions and support.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
